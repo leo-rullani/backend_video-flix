ed4d247c68b21ce404b7b8cf6cf7349b7d430f9d
Saranda Rullani <sarandarullani@macbook-pro-von-leo.home>
2025-12-09 14:01:08 +0100

    auth_app update urls.py

---
+++ auth/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import RegisterView, ActivateView
+
+app_name = "auth_api"
+
+urlpatterns = [
+    path("register/", RegisterView.as_view(), name="register"),
+    path("activate/<uidb64>/<token>/", ActivateView.as_view(), name="activate"),
+]
