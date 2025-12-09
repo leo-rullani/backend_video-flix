cda149c0edaacb15e4ba37f1333a2f11b18b360f
Saranda Rullani <sarandarullani@macbook-pro-von-leo.home>
2025-12-09 13:59:55 +0100

    serializers.py in auth_app

---
+++ auth/api/serializers.py
@@ -0,0 +1,32 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    """Serializer for user registration using email and password."""
+
+    password = serializers.CharField(write_only=True)
+    confirmed_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ("id", "email", "password", "confirmed_password")
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["confirmed_password"]:
+            raise serializers.ValidationError(
+                {"confirmed_password": "Passwords do not match."}
+            )
+        if User.objects.filter(email=attrs["email"]).exists():
+            raise serializers.ValidationError({"email": "Email already registered."})
+        return attrs
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        validated_data.pop("confirmed_password", None)
+        user = User(email=validated_data["email"], username=validated_data["email"], is_active=False)
+        user.set_password(password)
+        user.save()
+        return user
